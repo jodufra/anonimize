d57262eb2fa3ee3b402325e170711cda7ecd10b6
Joel Francisco <joel.francisco@zenn.pt>
2018-04-23 15:25:53 +0100

    Tests

---
+++ Tests.Anonimize/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Anonimize;
+
+namespace Tests.Anonimize
+{
+    [AnonimizeProperties(nameof(Name), nameof(Email))]
+    public class Entity
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public int Email { get; set; }
+
+    }
+}
